baf23df17cb588f364f8402106ea3d3c6b0b60b3
alpha-code-labs <138210698+alpha-code-labs@users.noreply.github.com>
2025-07-16 22:42:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const PORT = process.env.PORT || 5000;
 const allowedOrigins = [
   'http://localhost:3000',
   'https://jeemasters.alphacodelabs.com',
-  'www.jeemasters.alphacodelabs.com',
+  'https://www.jeemasters.alphacodelabs.com',
 ];
 
 // Middleware
